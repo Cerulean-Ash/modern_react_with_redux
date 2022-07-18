fa43481abb1f911a4675171dfe6dcf3a0c401d3f
Ashley Lampard <ashleylampard@live.com>
2022-07-18 06:43:18 +0100

    completed postsReducer user switch statement

---
+++ blog/src/reducers/postsReducer.js
@@ -0,0 +1,14 @@
+/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
+export default (state = [], action) => {
+  // if (action.type === "FETCH_POSTS") {
+  //   return action.payload;
+  // }
+
+  // return state;
+  switch (action.type) {
+    case "FETCH_POSTS":
+      return action.payload;
+    default:
+      return state;
+  }
+};
